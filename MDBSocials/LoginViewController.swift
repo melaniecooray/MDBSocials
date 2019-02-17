d5aad600009b868a364815d409f3c6da14937b98
Melanie Cooray <cooraymelanie@gmail.com>
2019-02-17 14:34:07 -0800

    Initial commit

---
+++ MDBSocials/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MDBSocials
+//
+//  Created by Melanie Cooray on 2/13/19.
+//  Copyright © 2019 Melanie Cooray. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
