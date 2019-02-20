39a64b13f945509cdd346b9122762acbd13b57d9
Tyler Reinecke <tylerreinecke@gmail.com>
2019-02-20 14:36:54 -0800

    Added file system, fixed merge conflict

---
+++ MDBSocials/Resources/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MDBSocials
+//
+//  Created by Tyler Reinecke on 2/20/19.
+//  Copyright © 2019 Melanie Cooray. All rights reserved.
+//
+
+import UIKit
+
+class Constants {
+
+}
