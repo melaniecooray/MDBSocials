5de07bf19d477861376f4a47d4ad7ff0d3e43efd
Melanie Cooray <cooraymelanie@gmail.com>
2019-02-21 11:19:27 -0800

    new social ui

--- MDBSocials/Controllers/NewSocialViewController/NewSocialViewController.swift
+++ MDBSocials/Controllers/NewSocialViewController/NewSocialViewController.swift
@@ -8,12 +8,52 @@
 
 import UIKit
 
-class NewSocialViewController: UIViewController {
+class NewSocialViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var nameOfEvent : UITextField!
+    var eventDescription : UITextField!
+    var imagePicker : UIImagePickerController!
+    var imageButton : UIButton!
+    var image : UIImageView!
+    var datePicker : UIDatePicker!
+    var selectedDate : String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         initUI()
+        setUpImagePicker()
+    }
+    
+    func setUpImagePicker() {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.image.image = info[.originalImage] as? UIImage
+    }
+    
+    @objc func uploadImage() {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func setUpDatePicker() {
+        datePicker = UIDatePicker()
+        datePicker.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 200)
+        datePicker.center = CGPoint(x: view.frame.width/2, y: eventDescription.frame.maxY + 50)
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.backgroundColor = .white
+        datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
+        view.addSubview(datePicker)
+    }
+    
+    @objc func datePickerValueChanged(_ sender: UIDatePicker){
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy hh:mm a"
+        let selectedDate: String = dateFormatter.string(from: sender.date)
+        print("Selected value \(selectedDate)")
     }
 }
